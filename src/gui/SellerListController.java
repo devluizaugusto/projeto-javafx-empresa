7a565735405ef7e2923d1b328ddefab842006a67
Luiz Augusto <dev.luiz.augusto@gmail.com>
2023-01-04 13:51:27 -0300

    implementado DepartmentComboBox pra escolher o departamento dos vendedores quando for inserir

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -31,6 +31,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerListController implements Initializable, DataChangeListener {
@@ -113,8 +114,9 @@ public class SellerListController implements Initializable, DataChangeListener {
 			Pane pane = loader.load();
 
 			SellerFormController controller = loader.getController();
-			controller.setDepartement(obj);
-			controller.setSellerService(new SellerService());
+			controller.setSeller(obj);
+			controller.setServices(new SellerService(), new DepartmentService());
+			controller.loadAssociateObjects();
 			controller.subscribeDataChangeListner(this);
 			controller.updateFormData();
 
